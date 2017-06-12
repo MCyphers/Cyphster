3b4d2091f80d0b1a8ecfb4a35c8dead7e9d3f644
Morgan Cyphers <morganlcyphers@gmail.com>
2017-06-12 22:38:02 +0000

    Lesson 29

--- Gemfile
+++ Gemfile
@@ -65,3 +65,5 @@ end
 
 gem "font-awesome-rails"
 gem "devise"
+gem "geocoder"
+gem "figaro"
--- app/models/place.rb
+++ app/models/place.rb
@@ -1,4 +1,11 @@
 class Place < ApplicationRecord
 	belongs_to :user
-	validates :name, presence: true
+	geocoded_by :address
+	after_validation :geocode
+
+	
+	validates :address, presence: true
+	validates :description, presence: true
+	validates :name, length: { minimum: 3,
+	too_short: "%{count} is the minimum allowed! No place has less than 3 letters!" }
 end
