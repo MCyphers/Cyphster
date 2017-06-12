def50b3a58519a8d4f3b09ce74c96a966411de5c
Morgan Cyphers <morganlcyphers@gmail.com>
2017-06-12 21:24:24 +0000

    Lesson 27

--- app/controllers/places_controller.rb
+++ app/controllers/places_controller.rb
@@ -10,8 +10,12 @@ class PlacesController < ApplicationController
 	end
 
 	def create
-		current_user.places.create(place_params)
-		redirect_to root_path
+		@place = current_user.places.create(place_params)
+		if @place.valid?
+			redirect_to root_path
+		else
+			render :new, status: :unprocessable_entity
+		end
 	end
 
 	def show
@@ -23,16 +27,23 @@ class PlacesController < ApplicationController
 
 		if @place.user !=current_user
 			return render text: 'Not Allowed', status: :forbidden
+		end
 	end
 
 	def update
 		@place = Place.find(params[:id])
+
+
 		if @place.user != current_user
 			return render text: 'Not Allowed', status: :forbidden
 		end
 
 		@place.update_attributes(place_params)
-		redirect_to root_path
+		if @place.valid?
+			redirect_to root_path
+		else
+			render :edit, status: :unprocessable_entity
+		end
 	end
 
 	def destroy
@@ -52,5 +63,4 @@ class PlacesController < ApplicationController
 	def place_params
 		params.require(:place).permit(:name, :description, :address)
 	end
-
 end
